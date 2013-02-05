60e39716e6eace0251e1c893c014fdf4d23dd500
Martin Billinger <flkazemakase@gmail.com>
2013-02-05 13:01:36 +0100

    BlockBase

--- src/BlockInterface.h
+++ src/BlockInterface.h
@@ -7,23 +7,36 @@
 #include <vector>
 #include <string>
 
+class BlockInterface;
+
+typedef void(*blockinput_callback_fpt)(BlockInterface*, const boost::any&);
+typedef void(*blockoutput_callback_fpt)(BlockInterface*, boost::any&);
+
 class Source
 {
 public:
 private:
-    std::string type_id( );
-    output_callback callback_func;
+    std::string label;
+    std::string type_id;
+    blockoutput_callback_fpt callback_func;
+};
+
+class Sink
+{
+public:
+private:
+    std::string label;
+    std::string type_id;
+    blockinput_callback_fpt callback_func;
 };
 
 // @todo call it BlockBase instead of BlockInterface?
 class BlockInterface
 {
 public:
-    typedef void(*input_callback)(BlockInterface*, const boost::any&);
-    typedef void(*output_callback)(BlockInterface*, boost::any&);
 
-    void addInput( input_callback func, std::string type_id );
-    void addOutput( output_callback func, std::string type_id );
+    void addInput( blockinput_callback_fpt func, const std::string &type_id, const std::string &label = "" );
+    void addOutput( blockoutput_callback_fpt func, const std::string &type_id, const std::string &label = "" );
 
     // example how to declare inputs/outputs
     //static void input( BlockInterface* this_ptr, const boost::any& buffer ) { }
@@ -31,6 +44,9 @@ public:
 
     size_t get_NrInputs( );
     size_t get_NrOutputs( );
+
+    Sink &getInput( size_t nr );
+    Source &getOutput( size_t nr );
 
 private:
     std::vector<Sink> inputs_;
